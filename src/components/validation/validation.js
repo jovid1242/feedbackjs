bad64a9c2d0c88284977ceb6ec9c5861edc8368b
jovid1242 <jovid1242@gmail.com>
2021-02-06 20:59:54 +0500

    update validation

--- src/components/validation/validation.js
+++ src/components/validation/validation.js
@@ -3,15 +3,15 @@ const validate = {
         if (!state.name) {
             return { error: true, message: 'Запольните поле Имя' }
         }
+        if (!state.phone) {
+            return { error: true, message: 'Запольните поле номер телефона  😉' };
+        }
         if (!state.email) {
             return { error: true, message: 'Запольните поле email' };
         }
         const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if (!re.test(state.email)) {
             return { error: true, message: 'Неверный адрес электронной почты' }
-        }
-        if (!state.phone) {
-            return { error: true, message: 'Запольните поле номер телефона  😉' };
         }
         // const phoneNum = /^((992|\+992)[\- ]?)?(\(?\d{3,4}\)?[\- ]?)?[\d\- ]{5,10}$/;
         // if (!phoneNum.test(state)) {
